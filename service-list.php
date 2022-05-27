3dc13e530b024f0218db6ad4c48e7c3077c81bfd
Rohit Verma <verma6641@gmail.com>
2022-05-27 21:53:43 +0530

    makeup artist sectoin added

--- service-list.php
+++ service-list.php
@@ -8,7 +8,7 @@
                     <ul class="salon-category-tabs">
                         <li data="salonsTab" class="active"> <span><img src="<?php echo $base_url; ?>/assets/images/salon-category-icon1.png" alt=""></span> Salons</li>
                         <li data="servicesTab"> <span><img src="<?php echo $base_url; ?>/assets/images/salon-category-icon2.png" alt=""></span> Services</li>
-                        <li data="makeupArtistTab"> <span><img src="<?php echo $base_url; ?>/assets/images/salon-category-icon3.png" alt=""></span> Makeup Artist</li>
+                        <li data="makeupArtistTab"> <span><img src="<?php echo $base_url; ?>/assets/images/salon-category-icon5.png" alt=""></span> Makeup Artist</li>
                         <li data="bridalTab"> <span><img src="<?php echo $base_url; ?>/assets/images/salon-category-icon3.png" alt=""></span> Bridal Makeup</li>
                         <li data="luxeTab"> <span><img src="<?php echo $base_url; ?>/assets/images/salon-category-icon4.png" alt=""></span> LUXE</li>
                     </ul>
@@ -31,26 +31,42 @@
             <div id="serviceData">
             </div>
         </div>
-        
+
         <div id="makeupArtistTab" class="custom-tab-content">
             <div id="makeupArtistData">
-                <div class="row"></div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="makeup-artist-box">
+                            <img src="/assets/images/artist-1.jpg" class="img-fluid" alt="">
+                            <p class="artist-name float-left">Anshika Sharma</p>
+                            <p class="rating float-right"> <i class="fas fa-star"></i> 4.8</p>
+                            <div class="clearfix"></div>
+                            <p class="float-left">42 booked</p>
+                            <p class="float-right">18 Reviews</p>
+                            <div class="clearfix"></div>
+                            <p><small>Makeup at your venue</small></p>
+                            <hr>
+                            <p class="price float-left"><i class="fas fa-rupee-sign"></i> 2000</p>
+                            <p class="artist-caption float-right">For Bride's Regular Makeup</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            
+
         </div>
 
         <div id="bridalTab" class="custom-tab-content">
             <div id="bridalData">
                 <div class="row"></div>
             </div>
-            
+
         </div>
 
         <div id="luxeTab" class="custom-tab-content">
             <div id="luxeData">
                 <div class="row"></div>
             </div>
-          
+
         </div>
 
     </div>
