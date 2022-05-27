4bf40acfb0199e69a49b8ce95eec9a0ea988d4d7
Rohit Verma <verma6641@gmail.com>
2022-05-27 21:26:38 +0530

    working on payment status pages

---
+++ payment-success.php
@@ -0,0 +1,14 @@
+<section class="section-padding bg-gray">
+  <div class="container">
+    <div class="payment-status-page">
+      <i class="fa fa-check-circle fa-4x text-success"></i>
+      <h2 class="text-success">Payment Successful</h2>
+      <p>Thank you your payment of Rs. 499 /- has been received.</p>
+
+      <div class="payment-status-btn-list">
+      <a href="#" class="btn btn-outline-secondary btn-lg">Continue Shopping</a>
+      <a href="#" class="btn btn-pink btn-lg">View Order</a>
+      </div>
+    </div>
+  </div>
+</section>
