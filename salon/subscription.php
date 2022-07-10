8886539cf015b7459a866033289883d415c69f2f
Rohit Verma <verma6641@gmail.com>
2022-07-10 15:29:53 +0530

    salon subscription design

--- salon/subscription.php
+++ salon/subscription.php
@@ -8,58 +8,82 @@
     <div class="row" id="dashboard-page">
         <div class="col-md-3 col-6">
             <div class="package-wrapper package-1">
-                <h2>Free</h2>
-                <ul>
-                    <li>Free Listing</li>
-                    <li>Branding of Your Salon</li>
-                    <li>Brand Promotions</li>
-                    <li>Free Leads & Queries</li>
-                    <li>24*7 Toll Free Number</li>
-                    <li>Discounts & Deals</li>
-                </ul>
-                <a href="#" class="btn btn-primary">Buy Now</a>
+                <div class="package-header">
+                    <p class="title">Standard</p>
+                    <h2>Rs. 0/</h2>
+                    <p class="duration">Per Month</p>
+                </div>
+                <div class="package-body">
+                    <ul>
+                        <li>Free Listing</li>
+                        <li>Branding of Your Salon</li>
+                        <li>Brand Promotions</li>
+                        <li>Free Leads & Queries</li>
+                        <li>24*7 Toll Free Number</li>
+                        <li>Discounts & Deals</li>
+                    </ul>
+                    <a href="#" class="btn">Buy Now</a>
+                </div>
             </div>
         </div>
         <div class="col-md-3 col-6">
-            <div class="package-wrapper package-1">
-                <h2>Silver</h2>
-                <ul>
-                    <li>Free Listing</li>
-                    <li>Branding of Your Salon</li>
-                    <li>Brand Promotions</li>
-                    <li>Free Leads & Queries</li>
-                    <li>24*7 Toll Free Number</li>
-                    <li>Discounts & Deals</li>
-                </ul>
-                <a href="#" class="btn btn-primary">Buy Now</a>
+            <div class="package-wrapper package-2">
+                <div class="package-header">
+                    <p class="title">Pro</p>
+                    <h2>Rs. 499/</h2>
+                    <p class="duration">Per Month</p>
+                </div>
+                <div class="package-body">
+                    <ul>
+                        <li>Free Listing</li>
+                        <li>Branding of Your Salon</li>
+                        <li>Brand Promotions</li>
+                        <li>Free Leads & Queries</li>
+                        <li>24*7 Toll Free Number</li>
+                        <li>Discounts & Deals</li>
+                    </ul>
+                    <a href="#" class="btn">Buy Now</a>
+                </div>
             </div>
         </div>
         <div class="col-md-3 col-6">
-            <div class="package-wrapper package-1">
-                <h2>Golden</h2>
-                <ul>
-                    <li>Free Listing</li>
-                    <li>Branding of Your Salon</li>
-                    <li>Brand Promotions</li>
-                    <li>Free Leads & Queries</li>
-                    <li>24*7 Toll Free Number</li>
-                    <li>Discounts & Deals</li>
-                </ul>
-                <a href="#" class="btn btn-primary">Buy Now</a>
+            <div class="package-wrapper package-3">
+                <div class="package-header">
+                    <p class="title">Pro Plus</p>
+                    <h2>Rs. 999/</h2>
+                    <p class="duration">Per Month</p>
+                </div>
+                <div class="package-body">
+                    <ul>
+                        <li>Free Listing</li>
+                        <li>Branding of Your Salon</li>
+                        <li>Brand Promotions</li>
+                        <li>Free Leads & Queries</li>
+                        <li>24*7 Toll Free Number</li>
+                        <li>Discounts & Deals</li>
+                    </ul>
+                    <a href="#" class="btn">Buy Now</a>
+                </div>
             </div>
         </div>
         <div class="col-md-3 col-6">
-            <div class="package-wrapper package-1">
-                <h2>Platinum</h2>
-                <ul>
-                    <li>Free Listing</li>
-                    <li>Branding of Your Salon</li>
-                    <li>Brand Promotions</li>
-                    <li>Free Leads & Queries</li>
-                    <li>24*7 Toll Free Number</li>
-                    <li>Discounts & Deals</li>
-                </ul>
-                <a href="#" class="btn btn-primary">Buy Now</a>
+            <div class="package-wrapper package-4">
+                <div class="package-header">
+                    <p class="title">Enterprise</p>
+                    <h2>Rs. 1499/</h2>
+                    <p class="duration">Per Month</p>
+                </div>
+                <div class="package-body">
+                    <ul>
+                        <li>Free Listing</li>
+                        <li>Branding of Your Salon</li>
+                        <li>Brand Promotions</li>
+                        <li>Free Leads & Queries</li>
+                        <li>24*7 Toll Free Number</li>
+                        <li>Discounts & Deals</li>
+                    </ul>
+                    <a href="#" class="btn">Buy Now</a>
+                </div>
             </div>
         </div>
 
